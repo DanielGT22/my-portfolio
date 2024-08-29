ae38e7d6a08092660332c4f52ed07e5fbd1ce664
Daniel Grujic <grujicdaniel22@gmail.com>
2024-08-29 17:23:16 +0200

    v2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,37 +1,62 @@
-import React from 'react';
-import { Container, Row, Col, Image, Button } from 'react-bootstrap';
+import React from "react";
+import { Container, Row, Col, Image, Button } from "react-bootstrap";
 
 const About = () => {
   return (
-    <Container id="about " className='py-4'>
-      <Row className="align-items-center">
-      
-        <Col xs={12} md={8}>
-          <h1>DANIEL GRUJIC</h1>
-          <hr></hr>
-          <div className='p-1'>
-          <Row>
-          <Col xs={6} md={3} className='p-1'>
-          <Button className='w-100   h-100 bg-dark border border-2 border-white ' style={{}}>Title</Button>
+    <div>
+      <Container id="about " className="py-4">
+        <Row className="align-items-center">
+          <Col xs={12} md={8}>
+            <h1>DANIEL GRUJIC</h1>
+            <hr></hr>
+            <div className="p-1">
+              <Row>
+                <Col xs={6} md={3} className="p-1">
+                  <Button
+                    className="w-100   h-100 bg-dark border border-2 border-white "
+                    style={{}}
+                  >
+                    Title
+                  </Button>
+                </Col>
+                <Col xs={6} md={3} className="p-1">
+                  <Button
+                    className="w-100   h-100 bg-dark border border-2 border-white "
+                    style={{}}
+                  >
+                    Projects
+                  </Button>
+                </Col>
+                <Col xs={6} md={3} className="p-1">
+                  <Button
+                    className="w-100   h-100 bg-dark border border-2 border-white "
+                    style={{}}
+                  >
+                    About Me
+                  </Button>
+                </Col>
+                <Col xs={6} md={3} className="p-1">
+                  <Button
+                    className="w-100   h-100 bg-dark border border-2 border-white "
+                    style={{}}
+                  >
+                    Contact
+                  </Button>
+                </Col>
+              </Row>
+            </div>
           </Col>
-          <Col xs={6} md={3} className='p-1'> 
-          <Button className='w-100   h-100 bg-dark border border-2 border-white ' style={{}}>Projects</Button>
+          <Col xs={12} md={4}>
+            <Image src="" roundedCircle className="" />
+            <p className="text-danger">A</p>
           </Col>
-          <Col xs={6} md={3} className='p-1'>
-          <Button className='w-100   h-100 bg-dark border border-2 border-white ' style={{}}>About Me</Button>
-          </Col>
-          <Col xs={6} md={3} className='p-1'>
-           <Button className='w-100   h-100 bg-dark border border-2 border-white ' style={{}}>Contact</Button>
-          </Col>
-          </Row>
-          </div>
-           </Col>
-        <Col xs={12} md={4}>
-          <Image src='' roundedCircle className="" />
-          <p className='text-danger'>A</p>
-        </Col>
-      </Row>
-    </Container>
+        </Row>
+      </Container>
+      <Container>
+        <h1>Lorem</h1>
+      </Container>
+    </div>
+    
   );
 };
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,45 +1,68 @@
-import React from 'react';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import React from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 const Contact = () => {
   return (
     <Container id="contact">
       <h2 className="text-start mb-4">Contact Me</h2>
-    
-      
-          <Form className=' custom-placeholder'>
-          <Row className="">
+
+      <Form className=" custom-placeholder">
+        <Row className="">
           <Col md={6}>
-          <Row className="">
-          <Col md={12} className="mb-2">
-            <Form.Group controlId="formName" className='' >
-            <Form.Label>Your Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter your name"   className="bg-dark border-2  " />
-            </Form.Group>
-            </Col>
+            <Row className="">
+              <Col md={12} className="mb-2">
+                <Form.Group controlId="formName" className="">
+                  <Form.Label>Your Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter your name"
+                    className="bg-dark border-2  "
+                  />
+                </Form.Group>
+              </Col>
             </Row>
             <Row className="">
-            <Col md={12} className="mb-1">
-            <Form.Group controlId="formEmail" className='bg-dark border-2 border-white' >
-            <Form.Label>Your Email</Form.Label>
-            <Form.Control type="text" placeholder="Enter your name"   className="bg-dark border-2  " />
-            </Form.Group>
-            </Col>
+              <Col md={12} className="mb-1">
+                <Form.Group
+                  controlId="formEmail"
+                  className="bg-dark border-2 border-white"
+                >
+                  <Form.Label>Your Email</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter your name"
+                    className="bg-dark border-2  "
+                  />
+                </Form.Group>
+              </Col>
             </Row>
-            </Col>
-            <Col md={6} >
-            <Form.Group controlId="formMessage" className='bg-dark border-2 border-white'>
-            <Form.Label>Your Message</Form.Label>
-              <Form.Control as="textarea" rows={5} placeholder="Enter your message" className='' />
+          </Col>
+          <Col md={6}>
+            <Form.Group
+              controlId="formMessage"
+              className="bg-dark border-2 border-white"
+            >
+              <Form.Label>Your Message</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={5}
+                placeholder="Enter your message"
+                className=""
+              />
             </Form.Group>
-            </Col>
-            </Row>
-            <div className='d-flex justify-content-center'>
-            <Button variant="dark" type="submit" className="mt-3 border-2 border-white w-25  ">Submit</Button>
-          </div>
-          </Form>
-       
-     
+          </Col>
+        </Row>
+        <div className="d-flex justify-content-center">
+          <Button
+            variant="dark"
+            type="submit"
+            className="mt-3 border-2 border-white w-25  "
+          >
+            Submit
+          </Button>
+        </div>
+        div
+      </Form>
     </Container>
   );
 };
