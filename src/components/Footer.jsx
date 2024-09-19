c01085d558a9e971920f65d8d9c962d353d11e00
Daniel Grujic <grujicdaniel22@gmail.com>
2024-09-19 15:42:58 +0200

    f

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,13 +14,14 @@ const Footer = () => {
   return (
     <div className="py-5 mb-0 bg-dark text-light">
       <Container>
+        <hr />
         <Row>
           {/* Contact Form Section */}
           <Col md={6} className="px-5">
             <h3 className="text-center mb-4" style={{ fontWeight: "600" }}>
               Get in Touch
             </h3>
-            <Form className="custom-placeholder bg-light p-4 rounded shadow-sm">
+            <Form className="custom-placeholder  p-4 rounded shadow-sm">
               <Row>
                 <Col md={12} className="mb-3">
                   <Form.Group controlId="formName">
@@ -54,7 +55,11 @@ const Footer = () => {
                   </Form.Group>
                 </Col>
                 <Col className="d-flex justify-content-center">
-                  <Button variant="primary" type="submit" className="px-4 py-2">
+                  <Button
+                    variant="dark"
+                    type="submit"
+                    className="px-4 py-2 border-white"
+                  >
                     Send Message
                   </Button>
                 </Col>
@@ -67,50 +72,71 @@ const Footer = () => {
             md={6}
             className="px-5 mt-5 mt-md-0 d-flex flex-column align-items-center"
           >
-            <h3 className="text-center mb-4" style={{ fontWeight: "600" }}>
+            <h3 className="text-center mb-5" style={{ fontWeight: "600" }}>
               Follow Me
             </h3>
             <div className="d-flex justify-content-center">
               <a
                 href="https://facebook.com"
-                className="text-light mx-3"
+                className="text-light mx-3 text-center text-decoration-none"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <FaFacebookF size={30} className="footer-icon" />
+                <FaFacebookF size={40} className="footer-icon" />
+                <br />
+                <p className="m-2">
+                  <strong className="text-decoration-none ">Facebook</strong>
+                </p>
               </a>
               <a
                 href="https://twitter.com"
-                className="text-light mx-3"
+                className="text-light mx-3 text-center text-decoration-none"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <FaTwitter size={30} className="footer-icon" />
+                <FaTwitter size={40} className="footer-icon" />
+                <br />
+                <p className="m-2">
+                  <strong className="text-decoration-none ">Twitter/X</strong>
+                </p>
               </a>
               <a
                 href="https://youtube.com"
-                className="text-light mx-3"
+                className="text-light mx-3 text-center text-decoration-none"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <FaYoutube size={30} className="footer-icon" />
+                <FaYoutube size={40} className="footer-icon" />
+                <br />
+                <p className="m-2">
+                  <strong className="text-decoration-none ">Youtube</strong>
+                </p>
               </a>
-              <a href="mailto:example@example.com" className="text-light mx-3">
-                <FaEnvelope size={30} className="footer-icon" />
+              <a
+                href="mailto:example@example.com"
+                className="text-light mx-3 text-center text-decoration-none"
+              >
+                <FaEnvelope size={40} className="footer-icon" />
+                <br />
+                <p className="m-2">
+                  <strong className="text-decoration-none ">Mail</strong>
+                </p>
               </a>
               <a
                 href="https://instagram.com"
-                className="text-light mx-3"
+                className="text-light mx-3 text-center text-decoration-none"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <FaInstagram size={30} className="footer-icon" />
+                <FaInstagram size={40} className="footer-icon" />
+                <br />
+                <p className="m-2">
+                  <strong className="text-decoration-none ">Instragram</strong>
+                </p>
               </a>
             </div>
           </Col>
         </Row>
-
-        <hr className="my-4" />
 
         {/* Back to Top and Copyright */}
         <Row className="text-center">
